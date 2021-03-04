efe94b003121c41116c1b71cdfccc27018fca43b
Jim LeBeau <jrlebeau@yahoo.com>
2021-03-04 13:39:25 -0800

    Initial Commit

---
+++ recipe list app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe list app
+//
+//  Created by James Lebeau on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
